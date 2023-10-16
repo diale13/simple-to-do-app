631d89b58e0fa1b63664abd6d538d73b33ae39bc
Diale13 <diegocapom@gmail.com>
2023-10-16 18:44:49 -0300

    :rocket: added CORS

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module github.com/diale13/simple-to-do-app
 
 go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.10 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/rs/cors v1.10.1
+)
--- backend/main.go
+++ backend/main.go
@@ -9,31 +9,46 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           600,
+	})
+
 	r.Use(middleware.Logger)
 	r.Use(JSONMiddleware)
-	r.Get("/",
-		func(w http.ResponseWriter, _ *http.Request) {
-			w.Write([]byte("Hello World!"))
-		})
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		printMonkeyLogo()
+		w.Write([]byte("Hello World!"))
+	})
 	r.Get("/api/tasks", getTasksFromJson)
 	r.Post("/api/tasks", createTask)
 	r.Put("/api/tasks/{taskId}", updateTasks)
 	r.Delete("/api/tasks/{taskId}", deleteTask)
-	printMonkeyLogo()
+
+	// Wrap your router with the CORS handler
+	handler := c.Handler(r)
+
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		http.ListenAndServe("localhost:9000", r)
+		// Start your server with the CORS-wrapped router
+		http.ListenAndServe("localhost:9000", handler)
 	}()
 
 	<-done
 	fmt.Println("Server stopped.")
 }
+
 func JSONMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
